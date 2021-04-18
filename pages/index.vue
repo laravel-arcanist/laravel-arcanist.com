377b844c2caa34f2cc8a0d93bc845e485a20df22
Kai Sassnowski <me@kai-sassnowski.com>
2021-04-18 23:44:58 +0200

    Add logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <p class="text-6xl text-indigo-700 font-serif font-bold tracking-wide">
-    Coming soon…
-  </p>
+  <div>
+    <p
+      class="text-3xl text-indigo-500 font-serif font-bold tracking-wider text-center"
+    >
+      coming soon
+    </p>
+
+    <img src="/logo@3x.png" class="w-64 opacity-20 mt-12" />
+  </div>
 </template>
